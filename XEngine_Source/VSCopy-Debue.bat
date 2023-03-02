3ea96ff8c83facbf6aefb468ce360292645ca2f7
qyt <486179@qq.com>
2023-03-02 14:07:55 +0800

    update:vscopy

--- XEngine_Source/VSCopy-Debue.bat
+++ XEngine_Source/VSCopy-Debue.bat
@@ -1,6 +1,5 @@
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_BaseLib.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_NetXApi.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_SystemApi.dll" "./"
-copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_DownLoad.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_XLog.dll" "./"
-copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIHelp.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIClient.dll" "./"
